e0813d9c1cc0625033d75865f499061bfc9992db
Schoen9576 <schoen9576@gmail.com>
2021-10-02 12:56:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: flase }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
 });
