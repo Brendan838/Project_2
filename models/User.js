a624d246478b9af962c0e67dbfef8eb2257ff6b2
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-10-02 11:14:26 -0500

    Added bcrypt require

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
     checkPassword(loginPassword) {
-        return
+        return bcrypt.compareSync(loginPassword, this.password);
     }
 }
