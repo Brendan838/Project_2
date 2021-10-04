7cb9d9cdf880a79a7a6e1f20ba2bd224b9ebb9bc
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-10-04 18:55:18 -0500

    added saved_code to Post model

--- models/Post.js
+++ models/Post.js
@@ -16,6 +16,10 @@ Post.init(
         type: DataTypes.STRING,
         allowNull: false,
       },
+      saved_code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       user_id: {
           type: DataTypes.INTEGER,
           references: {
