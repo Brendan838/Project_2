6001ee31ec4190d7ddae01d852404da25a834936
ristjosh <ristjosh1@icloud.com>
2021-10-06 19:56:42 -0500

    After login page

---
+++ public/script.js
@@ -0,0 +1,21 @@
+var cityList = [];
+var cityname;
+
+function renderCode(){
+    $("#codeList").empty();
+    $("#codeInput").val("");
+    
+    for (i=0; i<codeList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary code");
+        a.attr("data-name", codeList[i]);
+        a.text(codeList[i]);
+        $("#codeList").prepend(a);
+    } 
+}
+
+$("#codeInput").keypress(function(e){
+    if(e.which == 13){
+        $("#saveBtn").click();
+    }
+})
